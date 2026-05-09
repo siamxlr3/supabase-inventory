20f1b05bd23ac1dc4ed370351962bdc889eebeae
siamxlr3 <siamxlr3@gmail.com>
2026-05-10 04:05:39 +0600

    f38

--- src/models/order.ts
+++ src/models/order.ts
@@ -16,6 +16,7 @@ export interface Order {
   currency: string;
   location_id?: string;
   location?: any;
+  customer?: any;
   cancel_reason?: string;
   cancelled_at?: string;
   processed_at?: string;
